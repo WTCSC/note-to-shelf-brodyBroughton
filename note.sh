7a0b5732dacbfaa1d0b52746d06f60eddf47aefc
Brody <broughton.brody07@gmail.com>
2025-01-14 15:44:21 -0700

    Add welcome message and help function to note.sh

--- note.sh
+++ note.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+echo "Welcome to the Note Taking App Script File Pro Max Plus 3000 Ultra Super Edition with HyperDrive Quantum Technology and Turbocharged Performance"
+echo "Command: $1"
+
+Help()
+{
+    # Display Help
+    echo "Note taking app commands"
+    echo
+    echo "options:"
+    echo "note add \"Your note text here\"           Add a note"
+    echo "note list                  List all notes"
+    echo "note search \"keyword\"     Search for a note"
+}
